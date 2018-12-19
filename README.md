a3955b6ee89f2533aec8e0f3dc56dce2c6191d3f
James <jamesattfield1990@gmail.com>
2018-12-19 10:44:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# **Item Frame Protector**
+### A small bit of code to stop projectiles breaking item frames in sponge.
+
+## DISCLAIMER
+It is currently not very tested, nor extensible. It is intended to be a quick short term fix while other fixes are found. You can see the code, it's not a virus, just don't shout at me if it doesn't work when SpongeAPI changes etc. Although, that in mind, if you are using this, and it does no longer work, please get into contact and I can see if I can fix it. 
+
+ 
---
+++ src/main/java/co/pixelmc/itemframeprotector/ItemFrameProtector.java
@@ -0,0 +1,60 @@
+package co.pixelmc.itemframeprotector;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.Item;
+import org.spongepowered.api.entity.hanging.ItemFrame;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.CollideEntityEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = ItemFrameProtector.ID, version = ItemFrameProtector.VERSION, description = ItemFrameProtector.DESCRIPTION, authors = {"Obeliskthegreat - Founder of PixelMC.co"})
+public class ItemFrameProtector {
+    public static final String ID = "itemframeprotector";
+    public static final String VERSION = "1.0";
+    public static final String DESCRIPTION = "Stop item frames being destroyed by projectiles!";
+    
+    private static ItemFrameProtector itemFrameProtector;
+    public static ItemFrameProtector getInstance() {
+        return itemFrameProtector;
+    }
+    private static void setInstance(ItemFrameProtector i){
+        itemFrameProtector = i;
+    }
+
+    @Inject
+    private Logger logger;
+    @Inject
+    private Game game;
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    @Listener
+    public void onServerStarting(GameStartingServerEvent e){
+
+    }
+
+    @Listener
+    public  void onServerStarted(GameStartedServerEvent e){
+
+    }
+
+    @Listener
+    public void onEntityCollide(CollideEntityEvent event) {
+        for(Entity entity : event.getEntities())
+            if(entity instanceof ItemFrame) {
+                event.setCancelled(true);
+                break;
+            }
+    }
+}
